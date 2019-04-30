12c87b38c97b03ee5182974aeca4aff0e783b3aa
Ramkumar Rajaram <39878860+ramkumarrajaram@users.noreply.github.com>
2019-04-30 17:20:53 +0800

    Update GoogleWeather.js
    Update the fetch endpoint with open weather map api

--- src/components/GoogleWeather.js
+++ src/components/GoogleWeather.js
@@ -13,7 +13,7 @@ export default class GoogleWeather extends Component {
     }
 
     componentWillMount() {
-        fetch('http://localhost:8080')
+        fetch('http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22')
             .then(result => {
                 return result.json();
             }).then(data => {
