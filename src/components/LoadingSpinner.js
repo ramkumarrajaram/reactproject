8c0dcafd54e89d7d4c24f61e80caf00129d2cd2a
ramkumar_rajaram <ramkumar_rajaram@SIAQ00046MB.local>
2019-04-30 23:58:33 +0800

    Extract loading spinner to a separate logic

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import '../styles/Common.css'
+
+export default class LoadingSpinner extends Component {
+    render() {
+        return (
+
+            <div className="TestForm">
+                <p>
+                    <strong>Please wait!!! Loading data!!</strong>
+                </p>
+                <div className="lds-ring">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        )
+    }
+}
