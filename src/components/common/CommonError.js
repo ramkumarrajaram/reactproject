57a4686c6215f8ba1e901e2a902c2c3478c0f3df
Ramkumar Rajaram <ramkumar_rajaram@IBMCLONEs-MacBook-Pro-2.local>
2020-05-10 18:45:39 +0800

    Change the class components to functional components

--- src/components/common/CommonError.js
+++ src/components/common/CommonError.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import '../../styles/TestOrder.css'
 
-const CommonError = () => {
+const CommonError = (props) => {
     return (
         <div className="TestForm">
             <h2 className="TestCenter">An error occured!</h2>
             <div className="Summary-list">
                 <label><b>Error message: </b></label><br />
-                <label>{this.props.errorMessage.toUpperCase()}</label>
+                <label>{props.errorMessage.toUpperCase()}</label>
             </div>
             <span></span>
             <div className="TestCenter">
-                <input className="Button" type="submit" value="Back To Search" onClick={this.props.backToSearch} />
+                <input className="Button" type="submit" value="Back To Search" onClick={props.backToSearch} />
             </div>
         </div>
     );
--- src/components/order/OrderSummary.js
+++ src/components/order/OrderSummary.js
@@ -2,62 +2,63 @@ import React from 'react'
 import TestItOrderForm from "./TestItOrderForm";
 import '../../styles/TestOrder.css'
 
-export default class OrderSummary extends React.Component{
-
-    buildFieldsWithIndex = () => {
-
-        const copiesArray = this.props.state.copiesArray
-        let divList = [];
-
-        for (let index = 1; index <= 5; index++) {
-            divList.push(
-                <div className="Summary-list">
-                    <label><b>Number of copies for #{index}</b></label><br/>
-                    <label>{copiesArray[index] || 0}</label>
-                </div>
-            )
-        }
-
-        return divList;
-    }
-
-    render() {
-        return <div className="TestForm">
+const OrderSummary = (props) => {
+    return (
+        <div className="TestForm">
             <h2 className="TestCenter">Summary of your order</h2>
             <div className="Summary-list">
-                <label  ><b>Last Name</b></label><br/>
-                <label>{this.props.state.lastName}</label>
+                <label><b>Last Name</b></label><br/>
+                <label>{props.state.lastName}</label>
             </div>
             <div className="Summary-list">
                 <label><b>First Name</b></label><br/>
-                <label>{this.props.state.firstName}</label>
+                <label>{props.state.firstName}</label>
             </div>
             <div className="Summary-list">
                 <label><b>Phone</b></label><br/>
-                <label>{this.props.state.phoneNumber}</label>
+                <label>{props.state.phoneNumber}</label>
             </div>
             <div className="Summary-list">
                 <label><b>Country</b></label><br/>
-                <label>{this.props.state.country}</label>
+                <label>{props.state.country}</label>
             </div>
             <div className="Summary-list">
                 <label><b>Email</b></label><br/>
-                <label>{this.props.state.email}</label>
+                <label>{props.state.email}</label>
             </div>
             <div className="Summary-list">
                 <label><b>Address</b></label><br/>
-                <label>{this.props.state.address}</label>
+                <label>{props.state.address}</label>
             </div>
-            
-            {this.buildFieldsWithIndex()}
+
+            {buildFieldsWithIndex(props)}
 
             <div className="TestCenter">
-                <input className="Button" type="submit" value="Back To Order" onClick={this.backToOrder} />
+                <input className="Button" type="submit" value="Back To Order" onClick={backToOrder} />
             </div>
         </div>
+    );
+}
+
+const buildFieldsWithIndex = (props) => {
+
+    const copiesArray = props.state.copiesArray
+    let divList = [];
+
+    for (let index = 1; index <= 5; index++) {
+        divList.push(
+            <div className="Summary-list">
+                <label><b>Number of copies for #{index}</b></label><br/>
+                <label>{copiesArray[index] || 0}</label>
+            </div>
+        )
     }
 
-    backToOrder = () => {
-        return <TestItOrderForm />
-    }
+    return divList;
 }
+
+const backToOrder = () => {
+    return <TestItOrderForm />
+}
+
+export default OrderSummary;
